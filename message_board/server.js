88a99ea276f18cefa0858e1452bf5dfec7bebb4d
pbinneboese <pbinneboese@mac.com>
2017-04-10 18:43:23 -0700

    added 1955_api and message_board assignments

--- message_board/server.js
+++ message_board/server.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Require path
 var path = require('path');
 // Setting our Static & Views Folders
-// app.use(express.static(path.join(__dirname, './static')));
+app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
@@ -42,125 +42,58 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
 	Post.find({}, false, true).populate('_comments').exec(function(err, posts) {
 		if(err) { // error handling callback
-			console.log('DB read failure');
+			console.log('DB read failure', err.errors.name.message);
 			posts = {}; // blank out the posts
 		}
-		// for (i=0; i<posts.length; i++) {
-		// 	console.log('Post', i, 'Comments', posts[i]._comments.length);
-		// }
 		res.render('index', {posts:posts});
 	})
 })
 
-// 	// make a list of all posts & associated comments
-// 	var postComments = [{
-// 		_id: "",
-// 		name: "",
-// 		text: "",
-// 		_comments: [{
-// 			name: "",
-// 			text: ""
-// 		}],
-// 	}];
-// 	// fetch posts collection from database
-// 	Post.find({}, function(err, posts) {
-// 		if(err) { // error handling callback
-// 			console.log('DB read failure');
-// 			posts = {}; // blank out the posts
-// 		}
-// 		// for each post, find and attach the associated comments
-// 		for (i in posts) {
-// 			console.log(posts[i]);
-// 			postComments[i]._id = posts[i]._id;
-// 			postComments[i].name = posts[i].name;
-// 			postComments[i].text = posts[i].text;
-// 			for (k in posts[i]._comments) {
-// 				Comment.find({_id:posts[i]._comments[k]}, function(err, data) {
-// 					if(err) { // error handling callback
-// 						console.log('could not fetch comments');
-// 						data = {}; // blank out the comment
-// 					}
-// 					var comment = {name: data.name, text: data.text};
-// 					postComments[i]._comments[k].push(comment);
-// 				})
-// 			}
-// 		}
-// 		res.render('index', {posts:postComments});
-// 	})
-// })
-// for each post, find and attach the associated comments
-// 		for (i in posts) {
-// 			Post.findOne({_id: posts[i]._id})
-// 			.populate('_comments')
-// 			.exec(function(err, post) {
-// 				for (k in posts[i]._comments) {
-// 					console.log('post', posts[i]._comments[k]);
-// 					console.log('comment', post._comments[k]);
-// 					posts[i]._comments[k] = post._comments[k];
-// 				}
-// 			})
-// 		}
-// 		res.render('index', {posts:posts});
-// 	})
-// })
-
-
 // Create Post POST route
 app.post('/newpost', function(req, res) {
-	console.log("POST DATA", req.body);
+	// console.log("POST DATA", req.body);
 	var post = new Post({name:req.body.name, text: req.body.text});
 	// save to dB
 	post.save(function(err) {
 		if(err) { // error handling callback
-			console.log('post entry data not valid, please re-enter');
+			console.log('DB write failure', err.errors.name.message);
 		} else {
-			console.log('successfully added post', req.body.name);
-			res.redirect('/');
+			console.log('successfully added post', req.body.text);
 		}
+		res.redirect('/');
 	})
 })
 
 // route for creating one comment with the parent post id
-// app.post('/newcomment/:id', function (req, res) {
-// 	console.log("COMMENT DATA", req.body);
-// 	// fetch the post
-// 	Post.findOne({_id: req.params.id}, function(err, post) {
-// 		// create the comment
-// 		var comment = new Comment({name:req.body.name, text:req.body.text});
-// 		comment._post = post._id;
-// 		post._comments.push(comment);
-// 		comment.save(function(err) {
-// 			post.save(function(err) {
-// 				if(err) {
-// 					console.log('DB write failure');
-// 				}
-// 				else {
-// 					console.log('successfully added comment', req.body.text);
-// 					res.redirect('/');
-// 				}
-// 			})
-// 		})
-// 	})
-// })
-app.post("/newcomment/:id", function(req, res) {
+app.post('/newcomment/:id', function (req, res) {
+	// console.log("COMMENT DATA", req.body);
+	// fetch the post
 	Post.findOne({_id: req.params.id}, function(err, post) {
-		var newComment = new Comment({name: req.body.name, text: req.body.text});
-		newComment._post = post._id;
-		post.update({_id: post._id}, {$push: {"_comments": newComment}}, function(err) {
+		// create the comment
+		var comment = new Comment({name:req.body.name, text:req.body.text});
+		comment._post = post._id;
+		post._comments.push(comment);
+		// console.log('Comment', comment._id, comment._post);
+		// console.log('Post', post._id, post._comments);
+		comment.save(function(err) {
 			if(err) {
-				console.log('DB write failure1');
+				console.log('DB write failure', err.errors.name.message);
 			}
-		});
-		newComment.save(function(err){
-			if(err){
-				console.log('DB write failure2');
-			} else {
-				console.log('successfully added comment', req.body.text);
-				res.redirect("/");
+			else {
+				post.save(function(err) {
+					if(err) {
+						console.log('DB write failure', err.errors.name.message);
+					}
+					else {
+						console.log('successfully added comment', req.body.text);
+					}
+				})
 			}
-		});
-	});
-});
+		})
+		res.redirect('/');
+	})
+})
+
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
 	console.log("listening on port 8000");
